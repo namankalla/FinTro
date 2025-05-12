567f2938099f2bb04f9488803e078c5841bad3e1
Naman D. Kalla <naman.d.kalla@gmail.com>
2025-05-12 07:37:37 +0530

    Fix navigation and auth hooks

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,14 @@
 import { Stack } from "expo-router";
 import { AuthProvider } from "@/contexts/authContext";
 import { useEffect } from "react";
-import * as SplashScreen from 'expo-splash-screen';
+import * as SplashScreen from 'expo-splash-screen'
+import { useFrameworkReady } from '@/hooks/useFrameworkReady';
 
 // Prevent splash screen from auto-hiding
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  useFrameworkReady();
   useEffect(() => {
     // Hide splash screen after a delay
     const hideSplash = async () => {
